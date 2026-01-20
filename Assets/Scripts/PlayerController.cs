7e2aa847e98edeebe6b22ca293bf2e46a6f1b713
CRISvsGAME <cris@crisvsgame.com>
2026-01-20 20:00:00 +0000

    Roll a Ball 0.0.10
    Add Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
+    [SerializeField] private float speed = 10f;
+    private float movementX;
+    private float movementY;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
     private void Start()
     {
     }
@@ -12,8 +22,15 @@ public class PlayerController : MonoBehaviour
     {
     }
 
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(new Vector3(movementX, 0f, movementY) * speed);
+    }
+
     private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 }
